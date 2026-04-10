4d60cee2bd704cb5146f354eada4e652054028de
SRV <sauravextra@gmail.com>
2026-04-10 09:50:19 +0530

    217. Contains Duplicate

--- 217. Contains Duplicate.java
+++ 217. Contains Duplicate.java
@@ -23,3 +23,17 @@ class Solution {
 
     }
 }
+
+//3. Just sort
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int len = nums.length;
+
+        for(int i = 0; i < len - 1; i++){
+            if(nums[i] == nums[i+1]) return true;
+        }
+        return false;
+
+    }
+}
